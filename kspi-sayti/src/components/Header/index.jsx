27807236ed2b8d456a1dea8ec5851ba65d44d95b
Azimjon-m <azimeliboev2000@gmail.com>
2023-09-11 12:52:09 +0500

    Header Bitti

--- kspi-sayti/src/components/Header/index.jsx
+++ kspi-sayti/src/components/Header/index.jsx
@@ -1,11 +1,74 @@
-import React from 'react'
+import React from 'react';
+import './style.css'
+
+import { BsTelegram, BsFacebook, BsYoutube, BsFillTelephoneFill } from 'react-icons/bs';
+import { AiFillInstagram } from 'react-icons/ai';
+import { HiMail } from 'react-icons/hi';
 
 function Header() {
     return (
-        <div>
-            header qisim
+        <div className='h-[40px] md:h-[50px] flex justify-start xl:justify-center bg-[#012873] text-white style-overflow-x overflow-x-auto px-5 2xl:px-0' >
+            <div className='flex gap-x-4 xl:gap-x-10 items-center 2xl:justify-between 2xl:w-[1620px]'>
+                <ul className='flex font-medium gap-x-4 xl:gap-x-10 whitespace-nowrap select-none'>
+                    <li>
+                        <a href="www.google.com" target="_blank" rel="noopener noreferrer">
+                            HEMS TIZIMI
+                        </a>
+                    </li>
+                    <li>
+                        <a href="www.google.com" target="_blank" rel="noopener noreferrer">
+                            Insitut jurnali
+                        </a>
+                    </li>
+                    <li>
+                        <a href="www.google.com" target="_blank" rel="noopener noreferrer">
+                            Konferensilar
+                        </a>
+                    </li>
+                    <li>
+                        <a href="www.google.com" target="_blank" rel="noopener noreferrer">
+                            Ikkinchi ta'lim
+                        </a>
+                    </li>
+                    <li>
+                        <a href="www.google.com" target="_blank" rel="noopener noreferrer">
+                            Ochiq ma'lumotlar
+                        </a>
+                    </li>
+                </ul>
+                <div className='flex gap-x-4 lg:gap-x-1 xl:gap-x-4 items-center'>
+                    <span>
+                        <a href="www.google.com" target="_blank" rel="noopener noreferrer">
+                            <BsTelegram />
+                        </a>
+                    </span>
+                    <span>
+                        <a href="www.google.com" target="_blank" rel="noopener noreferrer">
+                            <AiFillInstagram className='text-[1.2rem]' />
+                        </a>
+                    </span>
+                    <span>
+                        <a href="www.google.com" target="_blank" rel="noopener noreferrer">
+                            <BsFacebook />
+                        </a>
+                    </span>
+                    <span>
+                        <a href="www.google.com" target="_blank" rel="noopener noreferrer">
+                            <BsYoutube className='text-[1.2rem]' />
+                        </a>
+                    </span>
+                    <span className='flex items-center'>
+                            <HiMail className='text-[1.4rem] me-1' />
+                            kspi_info@edu.uz
+                    </span>
+                    <span className='flex items-center'>
+                            <BsFillTelephoneFill className='me-1' />
+                            +9987354238
+                    </span>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default Header
+export default Header;
