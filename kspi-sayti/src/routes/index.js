ad9ba8a5c8ba6f178363ca77448159d0929b5cba
Azimjon-m <azimeliboev2000@gmail.com>
2023-09-12 09:57:46 +0500

    Institut Faoliyat ++

--- kspi-sayti/src/routes/index.js
+++ kspi-sayti/src/routes/index.js
@@ -1,8 +1,26 @@
 import React from "react";
 import Root from "../root/index";
 
+
 import Home from "../pages/Home";
-import About from "../pages/Insitut/InsitutKengashi";
+
+    // INSITUT
+import InsitutKengashi from "../pages/Insitut/InsitutKengashi";
+import InstitutHaqida from '../pages/Insitut/InsitutHaqida';
+import InstitutTuzilmasi from '../pages/Insitut/InsitutTuzilmasi';
+import Rekvizitlar from '../pages/Insitut/Rekvizitlar';
+import VirtualQabulhona from '../pages/Insitut/VirtualQabulxona';
+
+    // FAOLIYAT
+import JamoatchilikKengashiFaol from '../pages/Faoliyat/JamoatchilikKengashiFaol';
+import MadMarFaolyat from '../pages/Faoliyat/MadMarFaolyat';
+import OquvUslubiyFaolyat from '../pages/Faoliyat/OquvUslubiyFaolyat';
+import AkademikLitsey from '../pages/Faoliyat/AkademikLitsey';
+import IlmiyFaolyat from '../pages/Faoliyat/IlmiyFaolyat';
+import YoshlarBnIshlashMvaM from '../pages/Faoliyat/YoshlarBnIshlashMvaM';
+
+// import  from '';
+
 
 export const routes = [
     {
@@ -14,9 +32,53 @@ export const routes = [
                 path: '/'
             },
             {
-                element: <About />,
-                path: '/about'
-            }
+                element: <InsitutKengashi />,
+                path: '/insitut-kengashi'
+            },
+            {
+                element: <InstitutHaqida />,
+                path: '/institut-haqida'
+            },
+            {
+                element: <InstitutTuzilmasi />,
+                path: '/institut-tuzilmasi'
+            },
+            {
+                element: <Rekvizitlar />,
+                path: '/rekvizitlar'
+            },
+            {
+                element: <VirtualQabulhona />,
+                path: '/virtual-qabulhona'
+            },
+            {
+                element: <JamoatchilikKengashiFaol />,
+                path: '/jamoatchilik-kengashi-faol'
+            },
+            {
+                element: <MadMarFaolyat />,
+                path: '/mad-mar-faolyat'
+            },
+            {
+                element: <OquvUslubiyFaolyat />,
+                path: '/oquv-uslubiy-faolyat'
+            },
+            {
+                element: <AkademikLitsey />,
+                path: '/akademik-litsey'
+            },
+            {
+                element: <IlmiyFaolyat />,
+                path: '/ilmiy-faolyat'
+            },
+            {
+                element: <YoshlarBnIshlashMvaM />,
+                path: '/yoshlar-bn-ishlashMvaM'
+            },
+            // {
+            //     element: < />,
+            //     path: ''
+            // },
         ],
     },
 ];
