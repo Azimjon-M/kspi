c5c08622ba6debeb5ada311a0d74f1dec515665d
Azimjon-m <azimeliboev2000@gmail.com>
2023-09-12 14:33:03 +0500

    Navbar ++

--- kspi-sayti/src/components/Carousel/index.jsx
+++ kspi-sayti/src/components/Carousel/index.jsx
@@ -31,15 +31,15 @@ function Carousel() {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
         >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide><img src="https://kspi.uz/wp-content/uploads/2022/10/4B3A0824-scaled-2559x1437.jpg" alt="img" /></SwiperSlide>
+        <SwiperSlide><img src="https://kspi.uz/wp-content/uploads/2022/09/carousel_2-1248x701.jpg" alt="img" /></SwiperSlide>
+        {/* <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
         <SwiperSlide>Slide 5</SwiperSlide>
         <SwiperSlide>Slide 6</SwiperSlide>
         <SwiperSlide>Slide 7</SwiperSlide>
         <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide> */}
         </Swiper>
         </>
         );
