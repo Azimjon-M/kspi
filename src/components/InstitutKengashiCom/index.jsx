1c914e8389b2c54e65398a9fac21f2c255546ed7
Orifxon3595 <orifxontuychiyev@gmail.com>
2024-06-27 18:38:21 +0500

    update

--- src/components/InstitutKengashiCom/index.jsx
+++ src/components/InstitutKengashiCom/index.jsx
@@ -40,61 +40,66 @@ const InstitutKengashiCom = () => {
             <TextTranslate id="kengash" />
           </h2>
         </div>
-        <div>
-          <div className="card sm:card-side bg-base-100 shadow-xl w-full mx-auto mb-5">
-            <figure className="w-full sm:w-1/2 lg:w-1/3">
-              <img className="w-full" src={kotib} alt="kotib" />
-            </figure>
-            <div className="card-body xl:ml-10">
-              <h2 className="card-title text-xl md:text-xl xl:text-2xl">
-                <TextTranslate id="kengashKotib" />
-              </h2>
-              <p className="flex flex-col justify-center">
-                <p className="font-semibold text-lg md:text-2xl xl:text-3xl">
-                  <TextTranslate id="kengashKotibIsm" />
-                </p>
-                <p className="card-info">
-                  <p className="font-semibold md:text-lg xl:text-xl">
-                    <TextTranslate id="kengashKotibBoglanish" />{" "}
+        {data &&
+          data.map((item) => {
+            return (
+              <div>
+                <div className="card sm:card-side bg-base-100 shadow-xl w-full mx-auto mb-5">
+                  <figure className="w-full sm:w-1/2 lg:w-1/3">
+                    <img className="w-full" src={kotib} alt="kotib" />
+                  </figure>
+                  <div className="card-body xl:ml-10">
+                    <h2 className="card-title text-xl md:text-xl xl:text-2xl">
+                      <TextTranslate id="kengashKotib" />
+                    </h2>
+                    <p className="flex flex-col justify-center">
+                      <p className="font-semibold text-lg md:text-2xl xl:text-3xl">
+                        <TextTranslate id="kengashKotibIsm" />
+                      </p>
+                      <p className="card-info">
+                        <p className="font-semibold md:text-lg xl:text-xl">
+                          <TextTranslate id="kengashKotibBoglanish" />{" "}
+                        </p>
+                        <Link
+                          className="md:text-lg xl:text-xl"
+                          to="tel:+998 91 684 49 73"
+                        >
+                          <b>
+                            <TextTranslate id="kengashKotibTelefon" />:{" "}
+                          </b>
+                          +998 91 684 49 73
+                        </Link>
+                        <br />
+                        <Link
+                          className="md:text-lg xl:text-xl"
+                          to="mailto:example@mail.ru"
+                        >
+                          <b>
+                            <TextTranslate id="kengashKotibEmail" />:{" "}
+                          </b>
+                          example@mail.ru
+                        </Link>
+                      </p>
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <h2 className="text-center font-semibold text-lg md:text-xl lg:text-2xl xl:text-3xl mb-3">
+                    <TextTranslate id="kengashKotibMaqsad" />
+                  </h2>
+                  <p className="text-md md:text-lg lg:text-xl xl:text-lg mb-5">
+                    <TextTranslate id="kengashKotibMaqsadTavsif" />
                   </p>
-                  <Link
-                    className="md:text-lg xl:text-xl"
-                    to="tel:+998 91 684 49 73"
-                  >
-                    <b>
-                      <TextTranslate id="kengashKotibTelefon" />:{" "}
-                    </b>
-                    +998 91 684 49 73
-                  </Link>
-                  <br />
-                  <Link
-                    className="md:text-lg xl:text-xl"
-                    to="mailto:example@mail.ru"
-                  >
-                    <b>
-                      <TextTranslate id="kengashKotibEmail" />:{" "}
-                    </b>
-                    example@mail.ru
-                  </Link>
-                </p>
-              </p>
-            </div>
-          </div>
-          <div>
-            <h2 className="text-center font-semibold text-lg md:text-xl lg:text-2xl xl:text-3xl mb-3">
-              <TextTranslate id="kengashKotibMaqsad" />
-            </h2>
-            <p className="text-md md:text-lg lg:text-xl xl:text-lg mb-5">
-              <TextTranslate id="kengashKotibMaqsadTavsif" />
-            </p>
-            <h2 className="text-center font-semibold text-lg md:text-xl lg:text-2xl xl:text-3xl mb-3">
-              <TextTranslate id="kengashKotibKengash" />
-            </h2>
-            <p className="text-md md:text-lg lg:text-xl xl:text-lg mb-3">
-              <TextTranslate id="kengashKotibTavsif1" />
-            </p>
-          </div>
-        </div>
+                  <h2 className="text-center font-semibold text-lg md:text-xl lg:text-2xl xl:text-3xl mb-3">
+                    <TextTranslate id="kengashKotibKengash" />
+                  </h2>
+                  <p className="text-md md:text-lg lg:text-xl xl:text-lg mb-3">
+                    <TextTranslate id="kengashKotibTavsif1" />
+                  </p>
+                </div>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
