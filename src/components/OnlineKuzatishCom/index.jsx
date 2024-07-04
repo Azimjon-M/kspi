3e8445a6ffb17e04e13d36e1c4156be7e14e0913
Azimjon <azimeliboev2000@gmail.com>
2024-07-04 14:49:19 +0500

    OnlineKuzatishCom disign video ++

--- src/components/OnlineKuzatishCom/index.jsx
+++ src/components/OnlineKuzatishCom/index.jsx
@@ -44,7 +44,7 @@ const OnlineKuzatishCom = () => {
                             key={items.id}
                             className="collapse collapse-arrow my-1"
                         >
-                            <input type="checkbox" name="my-accordion-2" checked />
+                            <input type="checkbox" name="my-accordion-2" defaultChecked />
                             <div className="collapse-title text-xl font-medium bg-base-200">
                                 {items[`name_${isLang}`]}
                             </div>
@@ -55,7 +55,7 @@ const OnlineKuzatishCom = () => {
                                     )
                                     .map((item) => (
                                         <div
-                                            className={`h-[120px] sm:h-[263px] md:h-[318px] lg:h-[200px] xl:h-[270px] 2xl:h-[350px] w-full lg:w-[310px] xl:w-[390px] 2xl:w-[470px] lg:inline-block border mx-[2px] mt-2`}
+                                            className={`h-[165px] sm:h-[350px] md:h-[420px] lg:h-[230px] xl:h-[270px] 2xl:h-[320px] w-full lg:w-[310px] xl:w-[390px] 2xl:w-[480px] lg:inline-block border mx-[2px] mt-2`}
                                             key={item.id}
                                         >
                                             <h1 className="text-center font-medium my-1">
@@ -63,7 +63,73 @@ const OnlineKuzatishCom = () => {
                                             </h1>
                                             <iframe
                                                 title={item.name_uz}
-                                                className="w-full h-full border border-red-600"
+                                                className="w-full h-full"
+                                                src={item.link}
+                                                frameborder="0"
+                                                allowfullscreen
+                                            />
+                                        </div>
+                                    ))}
+
+{dataRekNom
+                                    ?.filter(
+                                        (item) => item.efirname_id === items.id
+                                    )
+                                    .map((item) => (
+                                        <div
+                                            className={`h-[165px] sm:h-[350px] md:h-[420px] lg:h-[230px] xl:h-[270px] 2xl:h-[320px] w-full lg:w-[310px] xl:w-[390px] 2xl:w-[480px] lg:inline-block border mx-[2px] mt-2`}
+                                            key={item.id}
+                                        >
+                                            <h1 className="text-center font-medium my-1">
+                                                {item[`name_${isLang}`]}
+                                            </h1>
+                                            <iframe
+                                                title={item.name_uz}
+                                                className="w-full h-full"
+                                                src={item.link}
+                                                frameborder="0"
+                                                allowfullscreen
+                                            />
+                                        </div>
+                                    ))}
+
+{dataRekNom
+                                    ?.filter(
+                                        (item) => item.efirname_id === items.id
+                                    )
+                                    .map((item) => (
+                                        <div
+                                            className={`h-[165px] sm:h-[350px] md:h-[420px] lg:h-[230px] xl:h-[270px] 2xl:h-[320px] w-full lg:w-[310px] xl:w-[390px] 2xl:w-[480px] lg:inline-block border mx-[2px] mt-2`}
+                                            key={item.id}
+                                        >
+                                            <h1 className="text-center font-medium my-1">
+                                                {item[`name_${isLang}`]}
+                                            </h1>
+                                            <iframe
+                                                title={item.name_uz}
+                                                className="w-full h-full"
+                                                src={item.link}
+                                                frameborder="0"
+                                                allowfullscreen
+                                            />
+                                        </div>
+                                    ))}
+
+{dataRekNom
+                                    ?.filter(
+                                        (item) => item.efirname_id === items.id
+                                    )
+                                    .map((item) => (
+                                        <div
+                                            className={`h-[165px] sm:h-[350px] md:h-[420px] lg:h-[230px] xl:h-[270px] 2xl:h-[320px] w-full lg:w-[310px] xl:w-[390px] 2xl:w-[480px] lg:inline-block border mx-[2px] mt-2`}
+                                            key={item.id}
+                                        >
+                                            <h1 className="text-center font-medium my-1">
+                                                {item[`name_${isLang}`]}
+                                            </h1>
+                                            <iframe
+                                                title={item.name_uz}
+                                                className="w-full h-full"
                                                 src={item.link}
                                                 frameborder="0"
                                                 allowfullscreen
