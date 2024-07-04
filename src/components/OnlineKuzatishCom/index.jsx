ad9cb5808c9cc3a23d6ba27485e3639675cdd1b8
Azimjon <azimeliboev2000@gmail.com>
2024-07-04 14:50:54 +0500

    OnlineKuzatishCom clean up

--- src/components/OnlineKuzatishCom/index.jsx
+++ src/components/OnlineKuzatishCom/index.jsx
@@ -44,78 +44,16 @@ const OnlineKuzatishCom = () => {
                             key={items.id}
                             className="collapse collapse-arrow my-1"
                         >
-                            <input type="checkbox" name="my-accordion-2" defaultChecked />
+                            <input
+                                type="checkbox"
+                                name="my-accordion-2"
+                                defaultChecked
+                            />
                             <div className="collapse-title text-xl font-medium bg-base-200">
                                 {items[`name_${isLang}`]}
                             </div>
                             <div className="collapse-content border border-base-200">
                                 {dataRekNom
-                                    ?.filter(
-                                        (item) => item.efirname_id === items.id
-                                    )
-                                    .map((item) => (
-                                        <div
-                                            className={`h-[165px] sm:h-[350px] md:h-[420px] lg:h-[230px] xl:h-[270px] 2xl:h-[320px] w-full lg:w-[310px] xl:w-[390px] 2xl:w-[480px] lg:inline-block border mx-[2px] mt-2`}
-                                            key={item.id}
-                                        >
-                                            <h1 className="text-center font-medium my-1">
-                                                {item[`name_${isLang}`]}
-                                            </h1>
-                                            <iframe
-                                                title={item.name_uz}
-                                                className="w-full h-full"
-                                                src={item.link}
-                                                frameborder="0"
-                                                allowfullscreen
-                                            />
-                                        </div>
-                                    ))}
-
-{dataRekNom
-                                    ?.filter(
-                                        (item) => item.efirname_id === items.id
-                                    )
-                                    .map((item) => (
-                                        <div
-                                            className={`h-[165px] sm:h-[350px] md:h-[420px] lg:h-[230px] xl:h-[270px] 2xl:h-[320px] w-full lg:w-[310px] xl:w-[390px] 2xl:w-[480px] lg:inline-block border mx-[2px] mt-2`}
-                                            key={item.id}
-                                        >
-                                            <h1 className="text-center font-medium my-1">
-                                                {item[`name_${isLang}`]}
-                                            </h1>
-                                            <iframe
-                                                title={item.name_uz}
-                                                className="w-full h-full"
-                                                src={item.link}
-                                                frameborder="0"
-                                                allowfullscreen
-                                            />
-                                        </div>
-                                    ))}
-
-{dataRekNom
-                                    ?.filter(
-                                        (item) => item.efirname_id === items.id
-                                    )
-                                    .map((item) => (
-                                        <div
-                                            className={`h-[165px] sm:h-[350px] md:h-[420px] lg:h-[230px] xl:h-[270px] 2xl:h-[320px] w-full lg:w-[310px] xl:w-[390px] 2xl:w-[480px] lg:inline-block border mx-[2px] mt-2`}
-                                            key={item.id}
-                                        >
-                                            <h1 className="text-center font-medium my-1">
-                                                {item[`name_${isLang}`]}
-                                            </h1>
-                                            <iframe
-                                                title={item.name_uz}
-                                                className="w-full h-full"
-                                                src={item.link}
-                                                frameborder="0"
-                                                allowfullscreen
-                                            />
-                                        </div>
-                                    ))}
-
-{dataRekNom
                                     ?.filter(
                                         (item) => item.efirname_id === items.id
                                     )
