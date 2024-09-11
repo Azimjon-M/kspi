5df8022eb9381ec4eb912b71e450d282f54b5af6
Orifxon3595 <orifxontuychiyev@gmail.com>
2024-09-11 21:52:50 +0500

    update students

--- src/components/MagistrDTSvaMalakaCom/index.jsx
+++ src/components/MagistrDTSvaMalakaCom/index.jsx
@@ -1,106 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TextTranslate from "../TextTranslate";
 import { LuDownload } from "react-icons/lu";
-import DTSvaMalakaPDF from "../../assets/pdf/DTSvaMalaka2020.pdf";
-
-const dataPDF = [
-  {
-    id: 1,
-    sana: "07.09.2023",
-    name: "Oliy ta'limning davlat ta'lim standarti 2020",
-    pdf: "../../assets/pdf/DTSvaMalaka2020.pdf",
-  },
-  {
-    id: 2,
-    sana: "07.09.2023",
-    name: "Oliy ta'limning davlat ta'lim standarti 2021",
-    pdf: "../../assets/pdf/DTSvaMalaka2020.pdf",
-  },
-  {
-    id: 3,
-    sana: "07.09.2023",
-    name: "60110300-Maktabgacha ta'lim psixologiyasi va pedagogikasi malaka talablari",
-    pdf: "../../assets/pdf/DTSvaMalaka2020.pdf",
-  },
-  {
-    id: 4,
-    sana: "07.09.2023",
-    name: "60112000-Chaqiriqqacha harbiy ta'lim malaka talablari",
-    pdf: "../../assets/pdf/DTSvaMalaka2020.pdf",
-  },
-  {
-    id: 5,
-    sana: "07.09.2023",
-    name: "60110100-Pedagogika(Qo'shimcha tarbiya fani o'qituvchisi) malaka talablari",
-    pdf: "../../assets/pdf/DTSvaMalaka2020.pdf",
-  },
-  {
-    id: 6,
-    sana: "07.09.2023",
-    name: "60110200-Maktabgacha ta'lim malaka talablari",
-    pdf: "../../assets/pdf/DTSvaMalaka2020.pdf",
-  },
-  {
-    id: 7,
-    sana: "07.09.2023",
-    name: "60110600-Matematika va informatika malaka talablari",
-    pdf: "../../assets/pdf/DTSvaMalaka2020.pdf",
-  },
-  {
-    id: 8,
-    sana: "07.09.2023",
-    name: "60110700-Fizika va astronomiya malaka talablari",
-    pdf: "../../assets/pdf/DTSvaMalaka2020.pdf",
-  },
-  {
-    id: 9,
-    sana: "07.09.2023",
-    name: "60110800-Kimyo malaka talablari",
-    pdf: "../../assets/pdf/DTSvaMalaka2020.pdf",
-  },
-  {
-    id: 10,
-    sana: "07.09.2023",
-    name: "60110300-Biologiya malaka talablari",
-    pdf: "../../assets/pdf/DTSvaMalaka2020.pdf",
-  },
-  {
-    id: 11,
-    sana: "07.09.2023",
-    name: "60111000-Geografiya va iqtisodiy bilim asoslari malaka talablari",
-    pdf: "../../assets/pdf/DTSvaMalaka2020.pdf",
-  },
-  {
-    id: 12,
-    sana: "07.09.2023",
-    name: "60111100-Tarix malaka talablari",
-    pdf: "../../assets/pdf/DTSvaMalaka2020.pdf",
-  },
-  {
-    id: 13,
-    sana: "07.09.2023",
-    name: "60111200-Tasviriy san'at va muxandislik grafikasi malaka talablari",
-    pdf: "../../assets/pdf/DTSvaMalaka2020.pdf",
-  },
-  {
-    id: 14,
-    sana: "07.09.2023",
-    name: "60111300-Musiqa ta'lim malaka talablari",
-    pdf: "../../assets/pdf/DTSvaMalaka2020.pdf",
-  },
-  {
-    id: 15,
-    sana: "07.09.2023",
-    name: "60111400-O'zbek tili adabiyoti malaka talablari",
-    pdf: "../../assets/pdf/DTSvaMalaka2020.pdf",
-  },
-];
+import { useSelector } from "react-redux";
+import APImDtsvaMalaka from "../../services/mDtsvaMalaka";
 
 function MagistrDTSvaMalakaCom() {
+  const Lang = useSelector((state) => state.reducerLang.isLang);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await APImDtsvaMalaka.get();
+        const sortedData = res.data.sort((a, b) => {
+          return new Date(b.sana) - new Date(a.sana);
+        });
+        setData(sortedData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
+
   return (
     <div className="max-w-7xl mx-auto py-16">
       <h1 className="text-md md: text-4xl font-bold text-[#004269] text-center">
-      <TextTranslate id="dTSvaMalakaTalablari"/>
+        <TextTranslate id="dTSvaMalakaTalablari" />
       </h1>
       <div className="relative shadow-md overflow-x-auto sm:rounded-lg mx-5 mt-10">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -110,38 +36,39 @@ function MagistrDTSvaMalakaCom() {
                 №
               </th>
               <th scope="col" className="px-2 md:px-6 py-4">
-              <TextTranslate id="DTSvaMalakaHujjatNomi"/>
+                <TextTranslate id="DTSvaMalakaHujjatNomi" />
               </th>
               <th scope="col" className="px-2 md:px-6 py-4 hidden md:block">
-              <TextTranslate id="DTSvaMalakaSana"/>
+                <TextTranslate id="DTSvaMalakaSana" />
               </th>
               <th scope="col" className="px-2 md:px-6 py-4">
-              <TextTranslate id="DTSvaMalakaBatafsil"/>
+                <TextTranslate id="DTSvaMalakaBatafsil" />
               </th>
             </tr>
           </thead>
           <tbody className="text-base">
-            {dataPDF &&
-              dataPDF.map((item) => {
-                const { id, sana, name } = item;
+            {data &&
+              data.map((item, index) => {
                 return (
                   <tr
-                    key={id}
+                    key={item.id}
                     className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 hover:bg-gray-200"
                   >
                     <th
                       scope="row"
                       className="pl-2 md:pl-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                     >
-                      {id}
+                      {index + 1}
                     </th>
-                    <td className="px-2 md:px-6 py-4">{name}</td>
+                    <td className="px-2 md:px-6 py-4">
+                      {item && item[`name_${Lang}`]}
+                    </td>
                     <td className="px-2 md:px-6 py-4 hidden md:block">
-                      {sana}
+                      {item.sana}
                     </td>
                     <td className="px-2 md:px-6 py-41">
                       <a
-                        href={DTSvaMalakaPDF}
+                        href={item.fayl}
                         className="text-blue-600 dark:text-blue-500"
                         target="blank"
                       >
