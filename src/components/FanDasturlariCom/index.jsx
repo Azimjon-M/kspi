1de04b0d2009b3d40585a40e17a30a41d6f75f53
Orifxon3595 <orifxontuychiyev@gmail.com>
2024-09-18 18:43:47 +0500

    update fan dasturlari

--- src/components/FanDasturlariCom/index.jsx
+++ src/components/FanDasturlariCom/index.jsx
@@ -66,7 +66,7 @@ const FanDasturlariCom = () => {
   }, [selectedTur]);
 
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-8 md:min-h-[calc(100vh-565px)] lg:min-h-[calc(100vh-400px)]">
       <h1 className="text-3xl font-bold text-center mb-8">Fan Dastur</h1>
 
       <div className="grid grid-cols-1 md:grid-cols-4 gap-6 mb-8">
