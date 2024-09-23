33dd2b349246c83029ec20f8ee45cedc55233c88
Azimjon-M <azimeliboev2000@gmail.com>
2024-09-23 09:34:22 +0500

    Gagistr fan dastur til++

--- src/components/MagistrFanDasturlariCom/index.jsx
+++ src/components/MagistrFanDasturlariCom/index.jsx
@@ -3,19 +3,19 @@ import APIBFanDasturlari from '../../services/mFanDasturlari';
 import APIBFanDasturlariKurs from '../../services/mFanDasturlariKurs';
 import APIBFanDasturlariTur from '../../services/mFanDasturlariTur';
 import APIBFanDasturlariYonalish from '../../services/mFanDasturlariYonalish';
-// import APIBFanDasturlariTalimTur from '../../services/bFanDasturlariTalimTur';
+import TextTranslate from '../TextTranslate';
+import { useSelector } from 'react-redux';
 
 const MagFanDAsturlari = () => {
   const [kurslar, setKurslar] = useState([]);
   const [yonalishlar, setYonalishlar] = useState([]);
   const [turlar, setTurlar] = useState([]);
   const [fanDasturlar, setFanDasturlar] = useState([]);
-  // const [talimTurlar, setTalimTurlar] = useState([]);
-
   const [selectedKurs, setSelectedKurs] = useState('');
-  // const [selectedTalimTur, setSelectedTalimTur] = useState('');
   const [selectedYonalish, setSelectedYonalish] = useState('');
   const [selectedTur, setSelectedTur] = useState('');
+  const isLang = useSelector((state) => state.reducerLang.isLang);
+  
 
   useEffect(() => {
     APIBFanDasturlariKurs.get()
@@ -23,27 +23,6 @@ const MagFanDAsturlari = () => {
       .catch(error => console.error('Error fetching kurslar:', error));
   }, []);
 
-  // useEffect(() => {
-  //   if (selectedKurs) {
-  //     APIBFanDasturlariTalimTur.get()
-  //       .then(response => {
-  //         // const filteredTalimTurlar = response.data.filter(item => item.fan_dastur_kurs_id === parseInt(selectedKurs));
-  //         // setTalimTurlar(filteredTalimTurlar);
-  //         setSelectedTalimTur(''); // Ta'lim turini bo'shatish
-  //         setSelectedYonalish(''); // Yo'nalishni bo'shatish
-  //         setTurlar([]); // Turlarni tozalash
-  //         setSelectedTur(''); // Turni bo'shatish
-  //       })
-  //       .catch(error => console.error('Error fetching talim turlar:', error));
-  //   } else {
-  //     // setTalimTurlar([]);
-  //     setSelectedTalimTur('');
-  //     setSelectedYonalish('');
-  //     setYonalishlar([]);
-  //     setTurlar([]);
-  //     setSelectedTur('');
-  //   }
-  // }, [selectedKurs]);
 
   useEffect(() => {
     if (selectedKurs) {
@@ -91,68 +70,51 @@ const MagFanDAsturlari = () => {
 
   return (
     <div className="max-w-7xl mx-auto px-4 py-8 md:min-h-[calc(100vh-565px)] lg:min-h-[calc(100vh-400px)]">
-      <h1 className="text-3xl font-bold text-center mb-8">Fan Dastur</h1>
-
+      <h1 className="text-3xl font-bold text-center mb-8"><TextTranslate id="fanDasturlari" /></h1>
       <div className="grid grid-cols-1 md:grid-cols-4 gap-6 mb-8">
         {/* Kurs Selection */}
         <div>
-          <label className="block text-sm font-medium text-gray-700">Kurs:</label>
+          <label className="block text-sm font-medium text-gray-700"><TextTranslate id="selectKurs" />:</label>
           <select 
             className="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm p-2"
             value={selectedKurs} 
             onChange={e => setSelectedKurs(e.target.value)}
           >
-            <option value="">Kursni tanlang</option>
+            <option value=""><TextTranslate id="inSelectKurs" /></option>
             {kurslar.map(kurs => (
-              <option key={kurs.id} value={kurs.id}>{kurs.name_uz}</option>
+              <option key={kurs.id} value={kurs.id}>{kurs[`name_${isLang}`]}</option>
             ))}
           </select>
         </div>
 
-        {/* Talim turi Selection */}
-        {/* <div>
-          <label className="block text-sm font-medium text-gray-700">Ta'lim turi:</label>
-          <select 
-            className="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm p-2"
-            value={selectedTalimTur} 
-            onChange={e => setSelectedTalimTur(e.target.value)}
-            disabled={!selectedKurs}
-          >
-            <option value="">Ta'lim turini tanlang</option>
-            {talimTurlar.map(talimTur => (
-              <option key={talimTur.id} value={talimTur.id}>{talimTur.name_uz}</option>
-            ))}
-          </select>
-        </div> */}
-
         {/* Yonalish Selection */}
         <div>
-          <label className="block text-sm font-medium text-gray-700">Yo'nalish:</label>
+          <label className="block text-sm font-medium text-gray-700"><TextTranslate id="selectYonalish" />:</label>
           <select 
             className="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm p-2" 
             value={selectedYonalish} 
             onChange={e => setSelectedYonalish(e.target.value)} 
             disabled={!selectedKurs}
           >
-            <option value="">Yo'nalishni tanlang</option>
+            <option value=""><TextTranslate id="inSelectYonalish" /></option>
             {yonalishlar.map(yonalish => (
-              <option key={yonalish.id} value={yonalish.id}>{yonalish.name_uz}</option>
+              <option key={yonalish.id} value={yonalish.id}>{yonalish[`name_${isLang}`]}</option>
             ))}
           </select>
         </div>
 
         {/* Tur Selection */}
         <div>
-          <label className="block text-sm font-medium text-gray-700">Tur:</label>
+          <label className="block text-sm font-medium text-gray-700"><TextTranslate id="selectTur" />:</label>
           <select 
             className="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm p-2" 
             value={selectedTur} 
             onChange={e => setSelectedTur(e.target.value)} 
             disabled={!selectedYonalish}
           >
-            <option value="">Turni tanlang</option>
+            <option value=""><TextTranslate id="inSelectTur" /></option>
             {turlar.map(tur => (
-              <option key={tur.id} value={tur.id}>{tur.name_uz}</option>
+              <option key={tur.id} value={tur.id}>{tur[`name_${isLang}`]}</option>
             ))}
           </select>
         </div>
@@ -160,18 +122,18 @@ const MagFanDAsturlari = () => {
 
       {/* Fan Dastur List */}
       <div className="bg-white shadow-md rounded-md p-6">
-        <h2 className="text-2xl text-center font-bold mb-4">Fan dasturlari ro'yxati</h2>
+        <h2 className="text-2xl text-center font-bold mb-4"><TextTranslate id="fanDasturlariRoyxati" /></h2>
         {fanDasturlar.length > 0 ? (
           <ul className="space-y-2">
             {fanDasturlar.map(fan => (
               <li key={fan.id} className="bg-gray-100 rounded-md p-3 flex justify-between items-center">
-                <a href={fan.fayl} target="_blank" rel="noopener noreferrer" className="text-indigo-600 font-medium hover:underline">{fan.name_uz}</a>
+                <a href={fan.fayl} target="_blank" rel="noopener noreferrer" className="text-indigo-600 font-medium hover:underline">{fan[`name_${isLang}`]}</a>
                 <span className="text-gray-500">{fan.sana}</span>
               </li>
             ))}
           </ul>
         ) : (
-          <p className="text-gray-500 text-center">Ma'lumot topilmadi :(</p>
+          <p className="text-gray-500 text-center"><TextTranslate id="malumotTopilmadi" /> :(</p>
         )}
       </div>
     </div>
