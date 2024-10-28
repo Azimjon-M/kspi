fd3d0b34d1514e60d324b915c654a9058ba94a32
Nodirbek Nurmamatov <103112982+nodirbek27@users.noreply.github.com>
2024-10-28 16:17:08 +0500

    add professor fikri

--- src/services/xalqaroProfessorFikri.js
+++ src/services/xalqaroProfessorFikri.js
@@ -1,6 +1,6 @@
 import axiosInstance from "./index";
 
-const ep = "faoliyat/professorlar_fikri/";
+const ep = "faoliyat/xalqaro_profesor_fikri/";
 
 const get = () => axiosInstance.get(ep);
 const getById = (id) => axiosInstance.get(`${ep}${id}/`);
