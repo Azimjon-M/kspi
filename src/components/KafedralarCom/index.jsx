f317d6bc279d5fab19628e7a607e8b75e3a8b185
Nodirbek Nurmamatov <103112982+nodirbek27@users.noreply.github.com>
2025-04-17 16:15:12 +0500

    update Tuzilma

--- src/components/KafedralarCom/index.jsx
+++ src/components/KafedralarCom/index.jsx
@@ -1,173 +1,64 @@
 import React, { useEffect, useState } from "react";
-import APITuzilmaFakultet from "../../services/tKafedra";
+import APITuzilmaRectorat from "../../services/tKafedra";
 import { useSelector } from "react-redux";
-import TextTranslate from "../TextTranslate";
+import Logo from "../../assets/images/logo_black.png";
+import { Link } from "react-router-dom";
 
 const Kafedra = () => {
-    const isLang = useSelector((state) => state.reducerLang.isLang);
+  const isLang = useSelector((state) => state.reducerLang.isLang);
+  const [dataRekLav, setDataRekLav] = useState([]);
 
-    const [dataKafLav, setDataKafLav] = useState([]);
-    const [datakafNom, setDataKafNom] = useState([]);
-    const [datakafHodim, setDataKafHodim] = useState([]);
+  const getDataRekLav = async () => {
+    await APITuzilmaRectorat.get()
+      .then((res) => {
+        setDataRekLav(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
 
-    const getDataKafLav = async () => {
-        await APITuzilmaFakultet.get()
-            .then((res) => {
-                setDataKafLav(res.data);
-            })
-            .catch((err) => console.log(err));
-    };
-    const getDataRekLNom = async () => {
-        await APITuzilmaFakultet.getR()
-            .then((res) => setDataKafNom(res.data))
-            .catch((err) => console.log(err));
-    };
-    const getDataRekLHodim = async () => {
-        await APITuzilmaFakultet.getH()
-            .then((res) => setDataKafHodim(res.data))
-            .catch((err) => console.log(err));
-    };
+  useEffect(() => {
+    getDataRekLav();
+  }, []);
 
-    useEffect(() => {
-        getDataKafLav();
-    }, []);
-    useEffect(() => {
-        getDataRekLNom();
-    }, []);
-    useEffect(() => {
-        getDataRekLHodim();
-    }, []);
+  // 4 tadan guruhlash funksiyasi
+  const chunkArray = (arr, size) => {
+    const chunks = [];
+    for (let i = 0; i < arr.length; i += size) {
+      chunks.push(arr.slice(i, i + size));
+    }
+    return chunks;
+  };
 
-    return (
-        <div className="flex flex-col justify-center items-center px-3 lg:pb-8 pb-4 py-4">
-            <h1 className="text-[24px] font-bold my-4 text-[#004269]">
-                Kafedralar
-            </h1>
-            {/* Mobil */}
-            <div className="w-full lg:hidden">
-                {
-                    dataKafLav?.map((item) => (
-                        <div
-                            key={item.id}
-                            className="collapse collapse-arrow my-1"
-                        >
-                            <input type="checkbox" name="my-accordion-2" />
-                            <div className="collapse-title text-xl font-medium bg-base-200">
-                                {item[`name_${isLang}`]}
-                            </div>
-                            <div className="collapse-content border border-base-200">
-                                {datakafNom
-                                    ?.filter(
-                                        (nomzod) =>
-                                            nomzod.kafedra_id === item.id
-                                    )
-                                    .map((nomzod) => (
-                                        <div
-                                            key={nomzod.id}
-                                            className="flex flex-col my-2"
-                                        >
-                                            <div className="flex justify-center">
-                                                <img
-                                                    className="rounded-full border w-[120px] object-cover h-[120px]"
-                                                    src={nomzod.rasm}
-                                                    alt="rahbar rasmi"
-                                                />
-                                            </div>
-                                            <h1>
-                                                <b>
-                                                    <TextTranslate id="tuzLav" />
-                                                    :
-                                                </b>
-                                                {` `}
-                                                {nomzod[`lavozim_${isLang}`]}
-                                            </h1>
-                                            <h1>
-                                                <b>
-                                                    <TextTranslate id="tuzFio" />
-                                                    :
-                                                </b>
-                                                {` `}
-                                                {nomzod[`fish_${isLang}`]}
-                                            </h1>
-                                            <h1>
-                                                <b>
-                                                    <TextTranslate id="tuzUnvon" />
-                                                    :
-                                                </b>
-                                                {` `}
-                                                {nomzod[`unvon_${isLang}`]}
-                                            </h1>
-                                            <h1>
-                                                <b>
-                                                    <TextTranslate id="tuzQabul" />
-                                                    :
-                                                </b>
-                                                {` `}
-                                                {
-                                                    nomzod[
-                                                        `qabul_soati_${isLang}`
-                                                    ]
-                                                }
-                                            </h1>
-                                            <h1>
-                                                <b>
-                                                    <TextTranslate id="tuzBio" />
-                                                    :
-                                                </b>
-                                                {` `}
-                                                {nomzod[`biografiya_${isLang}`]}
-                                            </h1>
-                                            <h1>
-                                                <b>
-                                                    <TextTranslate id="tuzTg" />
-                                                    :
-                                                </b>
-                                                {` `}
-                                                {nomzod.tg_username}
-                                            </h1>
-                                            <h1>
-                                                <b>
-                                                    <TextTranslate id="tuzTel" />
-                                                    :
-                                                </b>
-                                                {` `}+{nomzod.telefon_nomer}
-                                            </h1>
-                                        </div>
-                                    ))}
-                                {datakafHodim?.length !== 0 && (
-                                    <h1 className="text-center font-bold">
-                                        Hodimlar:{" "}
-                                    </h1>
-                                )}
-                                <div className="flex flex-wrap justify-center gap-2 w-full h-auto my-4">
-                                    {datakafHodim
-                                        ?.filter(
-                                            (hodim) =>
-                                                hodim.kafedra_id === item.id
-                                        )
-                                        .map((hodim) => (
-                                            <div
-                                                key={hodim.id}
-                                                className="w-[125px] h-auto border shadow-md overflow-hidden rounded-md p-1"
-                                            >
-                                                <div>
-                                                    <img
-                                                        src={hodim.rasm}
-                                                        alt="Hodim rasmi"
-                                                    />
-                                                </div>
-                                                <h1 className="text-center font-medium text-[#004369] text-[13px] mt-1">
-                                                    {hodim[`fish_${isLang}`]}
-                                                </h1>
-                                            </div>
-                                        ))}
-                                </div>
-                            </div>
-                        </div>
-                    ))}
+  return (
+    <div className="max-w-7xl mx-auto px-3 lg:pb-8 pb-4 py-4">
+      <h1 className="text-[24px] font-bold my-8 text-[#000]">Kafedralar</h1>
+
+      {/* Har 4 ta element alohida divda */}
+      {chunkArray(dataRekLav, 4).map((group, groupIndex) => (
+        <Link
+          key={groupIndex}
+        //   to="/:rektor"
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 w-full gap-4 mb-10 shadow-lg"
+        >
+          {group.map((item) => (
+            <div
+              key={item.id}
+              className="md:border-r p-4 flex flex-col items-center md:min-h-[250px]"
+            >
+              <img
+                src={Logo}
+                alt="University logo"
+                className="mb-4 w-[107px]"
+              />
+              <p className="text-center text-lg font-semibold text-[#5f4fa1]">
+                {item[`name_${isLang}`]}
+              </p>
             </div>
-        </div>
-    );
+          ))}
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default Kafedra;
