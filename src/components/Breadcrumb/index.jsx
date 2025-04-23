f240c64a98840827fcf39989349e66f3ea9f4bd7
Nodirbek Nurmamatov <103112982+nodirbek27@users.noreply.github.com>
2025-04-23 14:10:00 +0500

    breadcrumb++

--- src/components/Breadcrumb/index.jsx
+++ src/components/Breadcrumb/index.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import homeIcon from "../../assets/icons/home_icon.png";
 
 const Breadcrumb = ({ steps }) => {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto px-4 text-[#191932]">
       <div className="text-sm breadcrumbs">
-        <ul>
+        <ul className="flex items-end">
           {steps.map((step, index) => (
             <li key={index}>
               {step.link ? (
-                <Link to={step.link}>{step.text}</Link>
+                <Link
+                  className="font-poppins font-semibold text-[14px] flex items-center leading-[95%]"
+                  to={step.link}
+                >
+                  {index === 0 && (
+                    <img
+                      src={homeIcon}
+                      className="mr-[4px] mb-1 w-[18px] h-auto flex"
+                      alt="Home"
+                    />
+                  )}
+                  {step.text}
+                </Link>
               ) : (
-                <span className="inline-flex gap-2 items-center">
+                <span className="inline-flex gap-2 items-center font-semibold text-[14px] leading-[95%]">
                   {step.text}
                 </span>
               )}
--- src/components/RektoratCom/index.jsx
+++ src/components/RektoratCom/index.jsx
@@ -3,6 +3,8 @@ import APITuzilmaRectorat from "../../services/tRektorat";
 import { useSelector } from "react-redux";
 import Logo from "../../assets/images/logo_black.png";
 import { Link } from "react-router-dom";
+import Breadcrumb from "../Breadcrumb";
+import TextTranslate from "../TextTranslate";
 
 const Rektorat = () => {
   const isLang = useSelector((state) => state.reducerLang.isLang);
@@ -21,27 +23,39 @@ const Rektorat = () => {
   }, []);
 
   return (
-    <div className="max-w-7xl mx-auto px-3 lg:pb-8 pb-4 py-4">
-      <h1 className="text-[24px] font-bold my-8 text-[#000]">Rahbariyat</h1>
+    <div className="mb-10">
+      <div className="bg-[#EEF3F8]">
+        <Breadcrumb
+          steps={[
+            { text: <TextTranslate id="boshSahifa" />, link: "/" },
+            { text: "Rahbariyat" },
+          ]}
+        />
+      </div>
+      <div className="max-w-7xl mx-auto">
+        <h1 className="text-[24px] font-bold px-4 my-8 text-[#000]">
+          Rahbariyat
+        </h1>
 
-      {/* Har 4 ta element alohida divda */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-        {dataRekLav.map((lavozim) => (
-          <Link
-            key={lavozim.id}
-            //   to="/:rektor"
-            className="w-[309px] h-[329px] p-2 bg-[#191932] rounded-[10px] flex flex-col items-center"
-          >
-            <img
-              src={Logo}
-              alt="University logo"
-              className="mt-[20px] mb-4 w-[157px] h-auto"
-            />
-            <p className="text-center text-lg font-semibold text-[#fff]">
-              {lavozim[`name_${isLang}`]}
-            </p>
-          </Link>
-        ))}
+        {/* Har 4 ta element alohida divda */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 px-4">
+          {dataRekLav.map((lavozim) => (
+            <Link
+              key={lavozim.id}
+              to="rahbar"
+              className="min-h-[309px] p-2 bg-[#191932] rounded-[10px] flex flex-col items-center"
+            >
+              <img
+                src={Logo}
+                alt="University logo"
+                className="mt-[20px] mb-4 w-[157px] h-auto"
+              />
+              <p className="text-center text-lg font-semibold text-[#fff]">
+                {lavozim[`name_${isLang}`]}
+              </p>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
